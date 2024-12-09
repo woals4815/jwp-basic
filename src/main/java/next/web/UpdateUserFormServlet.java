d0e40fb1e26d27a88e4a618f7a2d47a50a268f4c
woals4815 <ckrt4815@gmail.com>
2024-12-09 16:27:56 +0900

    feature: UpdateUserFormServlet 추가

---
+++ src/main/java/next/web/UpdateUserFormServlet.java
@@ -0,0 +1,44 @@
+package next.web;
+
+import core.db.DataBase;
+import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/update")
+public class UpdateUserFormServlet extends HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(UpdateUserFormServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        User user = DataBase.findUserById(userId);
+
+        req.setAttribute("user", user);
+        RequestDispatcher rd = req.getRequestDispatcher("/user/update.jsp");
+        rd.forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        User user = DataBase.findUserById(userId);
+        user.setName(name);
+        user.setEmail(email);
+        user.setPassword(password);
+
+        resp.sendRedirect("/user/list");
+    }
+}
