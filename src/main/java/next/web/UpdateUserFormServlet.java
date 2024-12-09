368ea7103e870d8a11459d24b29e3815f199b043
woals4815 <ckrt4815@gmail.com>
2024-12-09 20:01:31 +0900

    fix: UpdateUserFormServlet 세션 로직 추가

--- src/main/java/next/web/UpdateUserFormServlet.java
+++ src/main/java/next/web/UpdateUserFormServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/user/update")
@@ -20,9 +21,28 @@ public class UpdateUserFormServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String userId = req.getParameter("userId");
-        User user = DataBase.findUserById(userId);
+        HttpSession session = req.getSession();
 
-        req.setAttribute("user", user);
+        if (userId == null || userId.isEmpty() || session == null) {
+            resp.sendRedirect("/user/login");
+            return;
+        }
+
+        Object value = session.getAttribute("user");
+        if (value == null) {
+            resp.sendRedirect("/user/login");
+            return;
+        }
+
+        User sessionUser = (User) value;
+        User existUser = DataBase.findUserById(userId);
+
+        if (!sessionUser.getUserId().equals(existUser.getUserId())) {
+            resp.sendRedirect("/");
+            return;
+        }
+
+        req.setAttribute("user", existUser);
         RequestDispatcher rd = req.getRequestDispatcher("/user/update.jsp");
         rd.forward(req,resp);
     }
