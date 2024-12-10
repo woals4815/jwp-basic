d34b6b8c385024d7df80a80297d5e26a541dd154
woals4815 <ckrt4815@gmail.com>
2024-12-10 17:28:18 +0900

    feature: DataAccessException 클래스 추가

---
+++ src/main/java/next/exception/DataAccessException.java
@@ -0,0 +1,16 @@
+package next.exception;
+
+public class DataAccessException extends RuntimeException {
+    public DataAccessException(String message) {
+        super(message);
+    }
+    public DataAccessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public DataAccessException(Throwable cause) {
+        super(cause);
+    }
+    public DataAccessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
