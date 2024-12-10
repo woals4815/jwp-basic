58c9d6b034400d2baabf3923160da7c3e001a9b8
woals4815 <ckrt4815@gmail.com>
2024-12-10 15:44:32 +0900

    feature: SelectJdbcTemplate class 추가

---
+++ src/main/java/next/dao/SelectJdbcTemplate.java
@@ -0,0 +1,73 @@
+package next.dao;
+import core.jdbc.ConnectionManager;
+import next.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelectJdbcTemplate {
+    public List<User> query() throws SQLException {
+        Connection con = null;
+        ResultSet rs = null;
+
+        try {
+            con = ConnectionManager.getConnection();
+            String sql = "SELECT * FROM USERS";
+            rs = con.prepareStatement(sql).executeQuery();
+            List<User> users = new ArrayList<>();
+            while(rs.next()) {
+                User newUser = new User(
+                        rs.getString("userId"),
+                        rs.getString("password"),
+                        rs.getString("name"),
+                        rs.getString("email")
+                );
+                users.add(newUser);
+            }
+            return users;
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+
+    public User queryForObject(String userId) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = ConnectionManager.getConnection();
+            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, userId);
+
+            rs = pstmt.executeQuery();
+
+            User user = null;
+            if (rs.next()) {
+                user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+                        rs.getString("email"));
+            }
+
+            return user;
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+}
