38b504a01aa9ec12b6bd4a9bb8699d65a7756b97
woals4815 <ckrt4815@gmail.com>
2024-12-10 16:01:41 +0900

    refactor: SelectJdbcTemplate 내에 query, queryObject sql로 받기

--- src/main/java/next/dao/SelectJdbcTemplate.java
+++ src/main/java/next/dao/SelectJdbcTemplate.java
@@ -10,25 +10,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SelectJdbcTemplate {
-    public List<User> query() throws SQLException {
+    public List<User> query(String sql) throws SQLException {
         Connection con = null;
         ResultSet rs = null;
-
         try {
             con = ConnectionManager.getConnection();
-            String sql = "SELECT * FROM USERS";
             rs = con.prepareStatement(sql).executeQuery();
-            List<User> users = new ArrayList<>();
-            while(rs.next()) {
-                User newUser = new User(
-                        rs.getString("userId"),
-                        rs.getString("password"),
-                        rs.getString("name"),
-                        rs.getString("email")
-                );
-                users.add(newUser);
-            }
-            return users;
+            return mapRow(rs);
         } finally {
             if (rs != null) {
                 rs.close();
@@ -39,13 +27,12 @@ public class SelectJdbcTemplate {
         }
     }
 
-    public User queryForObject(String userId) throws SQLException {
+    public User queryForObject(String sql, String userId) throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
         try {
             con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
             pstmt = con.prepareStatement(sql);
             setValues(userId, pstmt);
             rs = pstmt.executeQuery();
@@ -70,8 +57,23 @@ public class SelectJdbcTemplate {
         }
     }
 
-    public PreparedStatement setValues(String userId, PreparedStatement pstmt) throws SQLException {
-        pstmt.setString(1, userId);
+    public PreparedStatement setValues(String param, PreparedStatement pstmt) throws SQLException {
+        pstmt.setString(1, param);
         return pstmt;
+    }
+
+    public List<User> mapRow(ResultSet rs) throws SQLException {
+        List<User> users = new ArrayList<>();
+
+        while(rs.next()) {
+            User newUser = new User(
+                    rs.getString("userId"),
+                    rs.getString("password"),
+                    rs.getString("name"),
+                    rs.getString("email")
+            );
+            users.add(newUser);
+        }
+        return users;
     }
 }
